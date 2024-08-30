f2bcdd9c58ddb2cba9018e1e2d0192359929098d
Franco Prandi <francoprandi55@gmail.com>
2024-08-30 09:40:17 -0300

    feat: PWA configuration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import logo from "../assets/gevpLogo.png";
+import logo from "/assets/gevpLogo.png";
 import { useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "assets/gevpLogo.png",
+        "assets/entrenamientos2.png",
+        "assets/eventos.png",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "GEVP",
+        short_name: "GEVP",
+        description:
+          "Consulta o carga entrenamientos y eventos en el club GEVP.",
+        theme_color: "#06018a",
+        background_color: "#06018a",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "assets/gevpLogo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/gevpLogo-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
-    outDir: 'dist', // o tu directorio de salida preferido
+    outDir: "dist", // o tu directorio de salida preferido
   },
   server: {
     open: true,
