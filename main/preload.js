ef3ca308d2661ea03ec87a398883db261d72b33c
Franco Prandi <francoprandi55@gmail.com>
2024-09-10 12:18:17 -0300

    feat: electron app

---
+++ main/preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require("electron/renderer");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  on: (channel, callback) => {
+    ipcRenderer.on(channel, callback);
+  },
+  send: (channel, args) => {
+    ipcRenderer.send(channel, args);
+  },
+});
