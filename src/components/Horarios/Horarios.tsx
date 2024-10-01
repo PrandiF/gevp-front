62eadf238a894d2c5e27d89760b930c3c902d345
Franco Prandi <francoprandi55@gmail.com>
2024-10-01 17:04:57 -0300

    feat: views updates

--- src/components/Horarios/Horarios.tsx
+++ src/components/Horarios/Horarios.tsx
@@ -15,8 +15,8 @@ function Horarios() {
     <div className=" flex w-full items-start z-20 py-[8%]">
       <Header />
       <div className="flex w-full h-full items-start flex-col gap-8 xl:pt-0  pt-[5%]">
-        <div className="flex relative flex-col bg-[#fff] bg-opacity-90 z-20 w-[90%] items-center gap-10 xl:py-8 py-3 mx-auto xl:mt-[5%] mt-[15%]   rounded-3xl">
-          <div className="flex relative flex-col bg-[#000] bg-opacity-15 backdrop-blur-sm z-20 xl:w-[90%]  w-[95%] xl:px-5  items-center gap-10 xl:py-8 py-5 m-auto rounded-3xl xl:border-2 border border-gray-600">
+        <div className="flex relative flex-col bg-[#fff] bg-opacity-90 z-20 w-[95%] items-center gap-10 xl:py-8 py-3 mx-auto xl:mt-[5%] mt-[15%]   rounded-3xl">
+          <div className="flex relative flex-col bg-[#000] bg-opacity-15 backdrop-blur-sm z-20 xl:w-[90%]  w-[95%] xl:px-2  items-center gap-10 xl:py-8 py-5 m-auto rounded-3xl xl:border-2 border border-gray-600">
             <div
               className="flex mr-auto xl:pl-0 pl-5"
               data-aos="fade"
