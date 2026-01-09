6bb63980d73c525214bd58af223100eee2fa74f9
Franco Prandi <francoprandi55@gmail.com>
2026-01-09 18:21:20 -0300

    feat: login update

--- src/services/user.service.tsx
+++ src/services/user.service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_URL = `${import.meta.env.VITE_API_URL_DEV}/usuario`;
+const USER_URL = `https://gevp-back-api.onrender.com/api/usuario`;
 
 export const login = async (username: string, password: string) => {
   try {
